915d46df50db77869f1ea590056d76538192090d
Angelina Stilphen <astilphen000@west-mec.org>
2019-04-01 18:21:35 -0700

    finished outline, starting switch
    Finished the first javascript file called outline I also added css for the stylesheets using javascript.

--- bc_outline.js
+++ bc_outline.js
@@ -28,39 +28,74 @@
 */
 // generate outline based on h2 through h6 headings in the source document 
 
-window.addEventListener("load", makeOutline); 
+window.addEventListener("load", makeOutline);
 
 function makeOutline() {
       // location of the document outline
       var outline = document.getElementById("outline");
 
       // source document for outline 
-      var source = document.getElementById("doc"); 
+      var source = document.getElementById("doc");
 
       var mainHeading = document.createElement("h1");
       var outlineList = document.createElement("ol");
-      var headingText = document.createTextNode("Outline");  
+      var headingText = document.createTextNode("Outline");
 
-      mainHeading.appendChild(headingText); 
+      mainHeading.appendChild(headingText);
       outline.appendChild(mainHeading);
       outline.appendChild(outlineList);
 
       createList(source, outlineList);
-} .
+}
 
 function createList(source, outlineList) {
       // headings for te outline 
       var headings = ["H1", "H2", "H3", "H4", "H5", "H6"];
+      // previous level of the headings 
+      var prevLevel = 0;
+      // running total of the article headings 
+      var headNum = 0;
       // loop through all of the child nodes of source article until no child nodes are left 
       for (var n = source.firstChild; n !== null; n = n.nextSibling) {
             // examine only article headings
-            var headLevel = headings.indexOf(n.nodeName); 
+            var headLevel = headings.indexOf(n.nodeName);
 
             if (headLevel !== -1) {
+                  // add an id to the heading if it is messing 
+                  headNum++;
+                  if (n.hasAttribute("id") === false) {
+                        n.setAttribute("id", "head" + headNum);
+                  }
                   var listElem = document.createElement("li");
-                  listElem.innerHTML = n.firstChild.nodeValue;
-                  outlineList.appendChild(listElem);
+                  // create hypertext links to the document headings
+                  var linkElem = document.createElement("a");
+                  linkElem.innerHTML = n.innerHTML;
+                  linkElem.setAttribute("href", "#" + n.id); 
+                  // append the hypertext link to the list item 
+                  listElem.appendChild(linkElem);
+                  if (headLevel === prevLevel) {
+                        // append the list item to the current list
+                        outlineList.appendChild(listElem);
+                  } else if (headLevel > prevLevel) {
+                        // start a new nested list 
+                        var nestedList = document.createElement("ol");
+                        nestedList.appendChild(listElem);
+                        // appened nested list to lat item in the current list 
+                        outlineList.lastChild.appendChild(nestedList);
+                        // change the current list to the nested list 
+                        outlineList = nestedList;
+                  } else {
+                        // appdend the list item to a higher list
+                        // calculate the difference between the current and previous level
+                        var levelUp = prevLevel - headLevel;
+                        // go up to a higher level
+                        for (var i = 1; i <= levelUp; i++) {
+                              outlineList = outlineList.parentNode.parentNode;
+                        }
+                        outlineList.appendChild(listElem);
+                  }
+                  // update the value of prevLevel
+                  prevLevel = headLevel;
             }
       }
 }
-
